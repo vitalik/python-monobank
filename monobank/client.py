1ee50ea110bc9c7bdb3682a2777472e973bab8b4
Vitaliy Kucheriavyi <ppr.vitaly@gmail.com>
2020-01-04 18:07:13 +0200

    ✨ 🐱 ✨ pep8/black code format

--- monobank/client.py
+++ monobank/client.py
@@ -6,20 +6,19 @@ from monobank.transport import api_request
 
 
 class ClientBase(object):
-    
     def _get_headers(self, url):
-        raise NotImplementedError('Please implement _get_headers')
+        raise NotImplementedError("Please implement _get_headers")
 
     def make_request(self, method, path, **kwargs):
         headers = self._get_headers(path)
         return api_request(method, path, headers=headers, **kwargs)
 
     def get_currency(self):
-        return self.make_request('GET', '/bank/currency')
+        return self.make_request("GET", "/bank/currency")
 
     def get_client_info(self):
-        return self.make_request('GET', '/personal/client-info')
-    
+        return self.make_request("GET", "/personal/client-info")
+
     def get_statements(self, account, date_from, date_to=None):
         if date_to is None:
             date_to = date_from
@@ -30,14 +29,12 @@ class ClientBase(object):
             # in that case we moving 24 hours ahead to include desired date:
             date_to += timedelta(days=1)
         t_from, t_to = to_timestamp(date_from), to_timestamp(date_to)
-        
-        url = f'/personal/statement/{account}/{t_from}/{t_to}'
-        return self.make_request('GET', url)
-    
+
+        url = f"/personal/statement/{account}/{t_from}/{t_to}"
+        return self.make_request("GET", url)
+
     def create_webhook(self, url):
-        return self.make_request('POST', '/personal/webhook', json={
-            'webHookUrl': url,
-        })
+        return self.make_request("POST", "/personal/webhook", json={"webHookUrl": url,})
 
 
 class Client(ClientBase):
@@ -48,7 +45,7 @@ class Client(ClientBase):
 
     def _get_headers(self, url):
         return {
-            'X-Token': self.token,
+            "X-Token": self.token,
         }
 
 
@@ -58,21 +55,21 @@ class CorporateClient(ClientBase):
     def __init__(self, request_id, private_key):
         self.request_id = request_id
         self.key = SignKey(private_key)
-        
+
     def _get_headers(self, url):
         headers = {
-            'X-Key-Id': self.key.key_id(),
-            'X-Time': str(to_timestamp(datetime.now())),
-            'X-Request-Id': self.request_id,
+            "X-Key-Id": self.key.key_id(),
+            "X-Time": str(to_timestamp(datetime.now())),
+            "X-Request-Id": self.request_id,
         }
-        data = headers['X-Time'] + headers['X-Request-Id'] + url
-        headers['X-Sign'] = self.key.sign(data)
+        data = headers["X-Time"] + headers["X-Request-Id"] + url
+        headers["X-Sign"] = self.key.sign(data)
         return headers
-    
+
     def check(self):
         "Checks if user approved access request"
         try:
-            self.make_request('GET', '/personal/auth/request')
+            self.make_request("GET", "/personal/auth/request")
             return True
         except monobank.Error as e:
             if e.response.status_code == 401:
@@ -84,13 +81,13 @@ def access_request(permissions, private_key, callback_url=None):
     "Creates an access request for corporate api user"
     key = SignKey(private_key)
     headers = {
-        'X-Key-Id': key.key_id(),
-        'X-Time': str(to_timestamp(datetime.now())),
-        'X-Permissions': permissions,
+        "X-Key-Id": key.key_id(),
+        "X-Time": str(to_timestamp(datetime.now())),
+        "X-Permissions": permissions,
     }
     if callback_url:
-        headers['X-Callback'] = callback_url
-    path = '/personal/auth/request'
-    sign_str = headers['X-Time'] + headers['X-Permissions'] + path
-    headers['X-Sign'] = key.sign(sign_str)
-    return api_request('POST', path, headers=headers)
+        headers["X-Callback"] = callback_url
+    path = "/personal/auth/request"
+    sign_str = headers["X-Time"] + headers["X-Permissions"] + path
+    headers["X-Sign"] = key.sign(sign_str)
+    return api_request("POST", path, headers=headers)
--- monobank/transport.py
+++ monobank/transport.py
@@ -1,26 +1,26 @@
 import requests
 import monobank
 
-ENDPOINT = 'https://api.monobank.ua'
-UAGENT = 'python-monobank (https://github.com/vitalik/python-monobank, contact: ppr.vitaly@gmail.com)'
+ENDPOINT = "https://api.monobank.ua"
+UAGENT = "python-monobank (https://github.com/vitalik/python-monobank, contact: ppr.vitaly@gmail.com)"
 
 
 def api_request(method, path, **kwargs):
     "Handles all HTTP requests for monobank endponts"
-    headers = kwargs.pop('headers')
-    headers['User-Agent'] = UAGENT
+    headers = kwargs.pop("headers")
+    headers["User-Agent"] = UAGENT
     url = ENDPOINT + path
     # print(method, url, headers)
     response = requests.request(method, url, headers=headers, **kwargs)
-    
+
     if response.status_code == 200:
         if not response.content:  # can be just empty an response, but it's fine
             return None
         return response.json()
-    
+
     if response.status_code == 429:
         raise monobank.TooManyRequests("Too many requests", response)
-    
+
     data = response.json()
-    message = data.get('errorDescription', str(data))
+    message = data.get("errorDescription", str(data))
     raise monobank.Error(message, response)
